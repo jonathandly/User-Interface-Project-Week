89d573859b571410be9af0ff3c25c0c313b4763d
Jonathan Daly <dalyj138@gmail.com>
2019-02-13 15:45:57 -0500

    Added JS navigation expanded view design for desktop/mobile.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // JS goes here
+
+const close = '../img/nav-hamburger-close.png';
+const burger = '../img/nav-hamburger.png';
+const navbar = document.querySelector('.navbar');
+
+const toggleMenu = () => {
+    // Toggle the "menu-expand" class on the menu reference.
+    menu.classList.toggle('menu-expand');
+    if(menu.classList.contains('menu-expand')) {
+        menuButton.setAttribute('src', close);
+        navbar.setAttribute('style', 'background-color: rgba(119,136,153,0.7);');
+    } else {
+        menuButton.setAttribute('src', burger);
+        navbar.setAttribute('style', 'background-color: rgb(119,136,153);');
+    }
+
+}
+
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', () => toggleMenu());
